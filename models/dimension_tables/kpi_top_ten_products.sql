d93022272cf246ff4c86a2edae77a8352ae90c10
snehabiju2813 <snehabiju2813@gmail.com>
2025-09-02 14:55:21 +0530

    Files and folders updated

--- models/dimension_tables/kpi_top_ten_products.sql
+++
@@ -1,5 +0,0 @@
-select stock_code, sum(revenue) as tot_revenue
-from {{ ref("fct_data") }}
-group by stock_code
-order by tot_revenue desc
-limit 10
--- models/fact_tables/fct_data.sql
+++
@@ -1,3 +0,0 @@
-select invoice_no, stock_code, quantity, invoice_date, unit_price, customer_id, country,
-       (quantity * unit_price) as revenue
-       from {{ref("stg_clean_data")}}
